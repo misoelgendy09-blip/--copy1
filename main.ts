ce1e9c9c2d7ec256deb14d4a85f5c1e4b670068e
misoelgendy09-blip <misoelgendy09@gmail.com>
2026-04-09 19:35:17 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -346,15 +346,15 @@ img`
     `,
 img`
     e e e . . . . e e e . . . . 
-    c d d c . . c d d c . . . . 
-    c b d d f f d d b c . . . . 
-    c 3 b d d b d b 3 c . . . . 
-    f b 3 d d d d 3 b f . . . . 
-    e d d d d d d d d e . . . . 
-    e d f d d d d f d e . b f b 
-    f d d f d d f d d f . f d f 
-    f b d d b b d d 2 f . f d f 
-    . f 2 2 2 2 2 2 b b f f d f 
+    c d d c . . c d 3 c . . . . 
+    c b d d f f d 3 3 c . . . . 
+    c 3 b d d b 3 3 3 c . . . . 
+    f b 3 d d 3 3 3 b f . . . . 
+    e d d d 3 3 d d d e . . . . 
+    e d f 3 3 d d f d e . b f b 
+    f d 3 3 d d f d d f . f d f 
+    f 3 d d b b d d 2 f . f d f 
+    3 3 2 2 2 2 2 2 b b f f d f 
     . f b d d d d d d b b d b f 
     . f d d d d d b d d f f f . 
     . f d f f f d f f d f . . . 
