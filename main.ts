4769fe12c4975f01bfe9969f56f3e8f6c09f7ed7
misoelgendy09-blip <misoelgendy09@gmail.com>
2026-04-09 19:26:49 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -414,6 +414,152 @@ img`
     `
 ]
 textlist2 = ["Goodluck twin", "Don't mess up!", "Ouch! you stepped on my leg!"]
+let Asteroids = [
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c . . . . . . 
+    . . . . . . a b a a . . . . . . 
+    . . . . . c b a f c a c . . . . 
+    . . . . c b b b f f a c c . . . 
+    . . . . b b f a b b a a c . . . 
+    . . . . c b f f b a f c a . . . 
+    . . . . . c a a c b b a . . . . 
+    . . . . . . c c c c . . . . . . 
+    . . . . . . . c . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . c c c c . . 
+    . c c c c c . c c c c c f c c . 
+    c c a c c c c c 8 f f c f f c c 
+    c a f a a c c a f f c a a f f c 
+    c a 8 f a a c a c c c a a a a c 
+    c b c f a a a a a c c c c c c c 
+    c b b a a c f 8 a c c c 8 c c c 
+    . c b b a b c f a a a 8 8 c c . 
+    . . . . a a b b b a a 8 a c . . 
+    . . . . c b c a a c c b . . . . 
+    . . . . b b c c a b b a . . . . 
+    . . . . b b a b a 6 a . . . . . 
+    . . . . c b b b 6 6 c . . . . . 
+    . . . . . c a 6 6 b c . . . . . 
+    . . . . . . . c c c . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . c b a c . . . . . . 
+    . . . . c c b c f a c . . . . . 
+    . . . . a f b b b a c . . . . . 
+    . . . . a f f b a f c c . . . . 
+    . . . . c b b a f f c . . . . . 
+    . . . . . b b a f a . . . . . . 
+    . . . . . . c b b . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . c c c a c . . . . 
+    . . c c b b b a c a a a c . . . 
+    . c c a b a c b a a a b c c . . 
+    . c a b c f f f b a b b b a . . 
+    . c a c f f f 8 a b b b b b a . 
+    . c a 8 f f 8 c a b b b b b a . 
+    c c c a c c c c a b c f a b c c 
+    c c a a a c c c a c f f c b b a 
+    c c a b 6 a c c a f f c c b b a 
+    c a b c 8 6 c c a a a b b c b c 
+    c a c f f a c c a f a c c c b . 
+    c a 8 f c c b a f f c b c c c . 
+    . c b c c c c b f c a b b a c . 
+    . . a b b b b b b b b b b b c . 
+    . . . c c c c b b b b b c c . . 
+    . . . . . . . . c b b c . . . . 
+    `,
+img`
+    . . . . . . c c c . . . . . . . 
+    . . . . . a a a c c c . . . . . 
+    . . . c a c f a a a a c . . . . 
+    . . c a c f f f a f f a c . . . 
+    . c c a c c f a a c f f a c . . 
+    . a b a a c 6 a a c c f a c c c 
+    . a b b b 6 a b b a a c a f f c 
+    . . a b b a f f b b a a c f f c 
+    c . a a a c c f c b a a c f a c 
+    c c a a a c c a a a b b a c a c 
+    a c a b b a a 6 a b b 6 b b c . 
+    b a c b b b 6 b c . c c a c . . 
+    b a c c a b b a c . . . . . . . 
+    b b a c a b a a . . . . . . . . 
+    a b 6 b b a c . . . . . . . . . 
+    . a a b c . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . c b a c . . . . . . 
+    . . . . c c b c f a c . . . . . 
+    . . . . a f b b b a c . . . . . 
+    . . . . a f f b a f c c . . . . 
+    . . . . c b b a f f c . . . . . 
+    . . . . . b b a f a . . . . . . 
+    . . . . . . c b b . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . c c c c . . . . 
+    . . . . c c c c c c c c c . . . 
+    . . . c f c c a a a a c a c . . 
+    . . c c f f f f a a a c a a c . 
+    . . c c a f f c a a f f f a a c 
+    . . c c a a a a b c f f f a a c 
+    . c c c c a c c b a f c a a c c 
+    c a f f c c c a b b 6 b b b c c 
+    c a f f f f c c c 6 b b b a a c 
+    c a a c f f c a 6 6 b b b a a c 
+    c c b a a a a b 6 b b a b b a . 
+    . c c b b b b b b b a c c b a . 
+    . . c c c b c c c b a a b c . . 
+    . . . . c b a c c b b b c . . . 
+    . . . . c b b a a 6 b c . . . . 
+    . . . . . . b 6 6 c c . . . . . 
+    `,
+img`
+    . . . . . . . c c c a c . . . . 
+    . . c c b b b a c a a a c . . . 
+    . c c a b a c b a a a b c c . . 
+    . c a b c f f f b a b b b a . . 
+    . c a c f f f 8 a b b b b b a . 
+    . c a 8 f f 8 c a b b b b b a . 
+    c c c a c c c c a b c f a b c c 
+    c c a a a c c c a c f f c b b a 
+    c c a b 6 a c c a f f c c b b a 
+    c a b c 8 6 c c a a a b b c b c 
+    c a c f f a c c a f a c c c b . 
+    c a 8 f c c b a f f c b c c c . 
+    . c b c c c c b f c a b b a c . 
+    . . a b b b b b b b b b b b c . 
+    . . . c c c c b b b b b c c . . 
+    . . . . . . . . c b b c . . . . 
+    `
+]
 game.onUpdateInterval(1000, function () {
     if (level == 4) {
         asteroeid = sprites.createProjectileFromSide(img`
